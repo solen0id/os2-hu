e301c23a6ba3445d4cb54a040132178c9556622c
Max Patzelt <max.patzelt@gmail.com>
2023-11-26 15:00:25 +0100

    Add threaded client

--- exercise-2-async-chat/groupchat/src/bin/client.rs
+++ exercise-2-async-chat/groupchat/src/bin/client.rs
@@ -1,10 +1,49 @@
-use std::{io, env, net::TcpStream};
+use std::{
+    io::{self, Read, Write},
+    net, str, thread,
+};
 
 fn main() -> io::Result<()> {
-	let addr = env::args().nth(1).unwrap_or(String::from("127.0.0.1:8000"));
-	let mut _stream = TcpStream::connect(addr)?;
-	
-	// TODO: implement client logic
-	
-	Ok(())
+    let mut reader = net::TcpStream::connect("127.0.0.1:8000")?;
+    let mut buf = [0u8; 256];
+
+    let mut writer = reader.try_clone()?;
+
+    let handle = thread::spawn(move || loop {
+        let len = match reader.read(&mut buf) {
+            Ok(0) => break,
+            Ok(len) => len,
+            Err(_) => break,
+        };
+
+        let msg = match str::from_utf8(&buf[0..len]) {
+            Ok("!exit") => break,
+            Ok(msg) => msg,
+            Err(_) => break,
+        };
+
+        println!("recv: \"{}\"", msg);
+    });
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        writer.write(input.trim().as_bytes())?;
+
+        if input.trim() == "!exit" {
+            break;
+        }
+    }
+
+    match handle.join() {
+        Ok(_) => {
+            println!("Client exited successfully");
+            Ok(())
+        }
+        Err(e) => {
+            println!("Client exited with error: {:?}", e);
+            Ok(())
+        }
+    }
 }
--- exercise-2-async-chat/groupchat/src/bin/thread_server.rs
+++ exercise-2-async-chat/groupchat/src/bin/thread_server.rs
@@ -1,14 +1,26 @@
-use std::{io, env, net::TcpListener};
+use std::{
+    io::{self, Read, Write},
+    net::TcpListener,
+    thread,
+};
 
 fn main() -> io::Result<()> {
-	let addr = env::args().nth(1).unwrap_or(String::from("127.0.0.1:8000"));
-	let listener = TcpListener::bind(addr)?;
-	
-	// TODO: implement threads for the server logic
-	println!("Bound to {:?}", listener);
-	for stream in listener.incoming() {
-		println!("Connection attempt by {:?}", stream?);
-	}
-	
-	Ok(())
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+
+    println!("Server: listening on {:?}", listener);
+    while let Ok((mut stream, _)) = listener.accept() {
+        thread::spawn(move || {
+            let mut buf = [0u8; 256];
+
+            println!("Server: connected {:?}", stream);
+            while let Ok(bytes) = stream.read(&mut buf) {
+                if bytes == 0 || stream.write(&buf[0..bytes]).is_err() {
+                    break;
+                }
+            }
+            println!("Server: disconnected {:?}", stream);
+        });
+    }
+
+    Ok(())
 }
