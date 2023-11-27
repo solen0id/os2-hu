365e1f99be840d06db57a602f398b68018a49734
Max Patzelt <max.patzelt@gmail.com>
2023-11-27 13:19:54 +0100

    Add mpsc to thread sercer for message passing

--- exercise-2-async-chat/groupchat/src/bin/thread_server.rs
+++ exercise-2-async-chat/groupchat/src/bin/thread_server.rs
@@ -1,3 +1,5 @@
+use std::sync::mpsc;
+
 use std::{
     io::{self, Read, Write},
     net::TcpListener,
@@ -5,21 +7,48 @@ use std::{
 };
 
 fn main() -> io::Result<()> {
+    let mut conn_count = 0;
+    let mut clients = vec![];
+
     let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let (tx, rx) = mpsc::channel();
 
     println!("Server: listening on {:?}", listener);
+
     while let Ok((mut stream, _)) = listener.accept() {
+        let tx_clone = tx.clone();
+        let stream_clone = stream.try_clone()?;
+        clients.push(stream_clone);
+
         thread::spawn(move || {
+            let conn_id = conn_count.clone();
             let mut buf = [0u8; 256];
 
             println!("Server: connected {:?}", stream);
-            while let Ok(bytes) = stream.read(&mut buf) {
-                if bytes == 0 || stream.write(&buf[0..bytes]).is_err() {
+
+            loop {
+                let len = match stream.read(&mut buf) {
+                    Ok(0) => break,
+                    Ok(len) => len,
+                    Err(_) => break,
+                };
+                let msg = String::from_utf8((&buf[0..len]).to_vec()).unwrap();
+
+                if tx_clone.send(msg).is_err() {
                     break;
                 }
             }
+
             println!("Server: disconnected {:?}", stream);
         });
+
+        conn_count += 1;
+
+        for msg in &rx {
+            for mut client in &clients {
+                client.write(msg.as_bytes()).unwrap();
+            }
+        }
     }
 
     Ok(())
