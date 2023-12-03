c01b463e726625cbc2d1370b4b141cc6349f1574
tohi211 <150695116+tohi211@users.noreply.github.com>
2023-12-03 23:15:10 +0100

    Update event server
    added disconnecting via !exit
    fixed input>buffer
    fixed connection loss

--- exercise-2-async-chat/groupchat/src/bin/event_server.rs
+++ exercise-2-async-chat/groupchat/src/bin/event_server.rs
@@ -1,70 +1,121 @@
 #![allow(unused_imports)]
 use mio::{net::TcpListener, Events, Interest, Poll, Token};
 use std::{
-    env,
-    io::{self, ErrorKind, Read, Write},
+	env,
+	io::{self, ErrorKind, Read, Write},
+	net::Shutdown,
+	str,
 };
+use futures::future::err;
+use mio::net::TcpStream;
 
 fn main() -> io::Result<()> {
-    let addr = env::args().nth(1).unwrap_or(String::from("127.0.0.1:8000"));
-    let mut listener = TcpListener::bind(addr.parse().unwrap())?;
+	let mut events = Events::with_capacity(1024);
+	let mut poll = Poll::new()?;
+	let mut clients: Vec<Option<TcpStream>> = Vec::new();
+	let mut messages: Vec<String> = Vec::new();
+	let mut buf = [0u8; 256];
 
-    // TODO: implement an event loop for the server logic
-    let mut events = Events::with_capacity(4);
-    let mut poll = Poll::new()?;
-    let mut clients = vec![];
-    let mut buf = [0u8; 4];
+	let addr = env::args().nth(1).unwrap_or(String::from("127.0.0.1:8000"));
+	let mut listener = TcpListener::bind(addr.parse().unwrap())?;
+	println!("Server: listening on {:?}", listener);
 
-    poll.registry()
-        .register(&mut listener, Token(0), Interest::READABLE)?;
-    loop {
-        poll.poll(&mut events, None)?;
+	poll.registry()
+		.register(&mut listener, Token(0), Interest::READABLE)?;
 
-        for event in events.iter() {
-            match event.token() {
-                Token(0) => {
-                    while let Ok((mut stream, _)) = listener.accept() {
-                        poll.registry().register(
-                            &mut stream,
-                            Token(clients.len() + 1),
-                            Interest::READABLE,
-                        )?;
-                        clients.push(Some(stream));
-                    }
-                }
+	loop {
+		poll.poll(&mut events, None)?;
 
-                Token(n) => {
-                    let mut bytes = 0;
-                    let stream = clients[n - 1].as_mut().unwrap();
-                    let disconnect = loop {
-                        match stream.read(&mut buf) {
-                            Ok(b) => {
-                                bytes = b;
-                                if bytes == 0 {
-                                    break true;
-                                }
-                            }
-                            Err(e) => break e.kind() != ErrorKind::WouldBlock,
-                        }
-                    };
-                    // diconnect by empty message TOOO add !exit
-                    if disconnect {
-                        poll.registry().deregister(stream)?;
-                        clients[n - 1] = None;
-                    }
-                    // Send message to all connected clients
-                    for i in 0..clients.len() {
-                        // skip disconnected clients
-                        if !clients[i].is_none() {
-                            let receiver = clients[i].as_mut().unwrap();
-                            if receiver.write(&buf[0..bytes]).is_err() {
-                                // TODO Fehlerbehandlung beim schreiben
-                                continue;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+		for event in events.iter() {
+			match event.token() {
+				Token(0) => {
+					while let Ok((mut stream, _)) = listener.accept() {
+						let mut token_position = clients.len()+1;
+						// Check if we can reuse a previous ID
+						for i in 0..clients.len() {
+							if clients[i].is_none() {
+								token_position = i+1;
+							}
+						}
+						println!("Server: connected {:?}", stream);
+						poll.registry().register(
+							&mut stream,
+							Token(token_position),
+							Interest::READABLE,
+						)?;
+						// Create new slot
+						if token_position>clients.len(){
+							clients.push(Some(stream));
+							messages.push(String::new());
+
+						}
+						// Reuse previous slot
+						else {
+							clients[token_position-1] = Some(stream);
+							messages[token_position-1].clear();
+						}
+					}
+				}
+
+				Token(n) => {
+					let mut input = String::new();
+					let disconnect = loop{
+						match clients[n-1].as_mut().unwrap().read(&mut buf) {
+							Ok(b) => {
+								if b == 0 {
+									break true;
+								}
+								input.push_str(str::from_utf8(&buf[0..b]).unwrap());
+								// send message to all clients when it is complete
+								if input.ends_with("\n"){
+									if input.trim() == "!exit" {
+										break true;
+									}
+									input = format!("Client {}: {}", n-1, input);
+									for i in 0..clients.len() {
+										// skip disconnected clients
+										if !clients[i].is_none() {
+											let receiver = clients[i].as_mut().unwrap();
+											if receiver.write(input.as_bytes()).is_err() {
+												let _ = receiver.shutdown(Shutdown::Both);
+												continue;
+											}
+										}
+									}
+									input.clear();
+								}
+							}
+							Err(e) => {
+								// Close connection in case of connection errors
+								if  e.kind() == ErrorKind::ConnectionReset ||
+									e.kind() == ErrorKind::ConnectionAborted ||
+									e.kind() == ErrorKind::NotConnected {
+									break true;
+								}
+								break e.kind() != ErrorKind::WouldBlock
+							}
+						}
+					};
+					if disconnect {
+						// Try to send !exit to client
+						input = format!("!exit\n");
+						let _ = clients[n - 1].as_mut().unwrap().write(input.as_bytes());
+						// Delete connection
+						println!("Server: disconnected {:?}", clients[n-1]);
+						poll.registry().deregister(clients[n-1].as_mut().unwrap())?;
+						clients[n - 1] = None;
+						// Send disconnected message to other clients
+						input = format!("Client {} disconnected\n", n-1);
+						for i in 0..clients.len() {
+							// skip disconnected clients
+							if !clients[i].is_none() {
+								let receiver = clients[i].as_mut().unwrap();
+								let _ = receiver.write(input.as_bytes());
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
