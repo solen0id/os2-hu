8123ce17be4928fd0494a9c3c01658929c8ec203
Max Patzelt <max.patzelt@gmail.com>
2024-02-06 14:17:25 +0100

    Formatting

--- exercise-5-consensus/src/protocol.rs
+++ exercise-5-consensus/src/protocol.rs
@@ -4,64 +4,74 @@ use crate::network::Channel;
 /// Message-type of the network protocol.
 #[derive(Debug)]
 pub enum Command {
-	/// Open an account with a unique name.
-	Open { account: String },
-	
-	/// Deposit money into an account.
-	Deposit { account: String, amount: usize },
-	
-	/// Withdraw money from an account.
-	Withdraw { account: String, amount: usize },
-	
-	/// Transfer money between accounts.
-	Transfer { src: String, dst: String, amount: usize },
-	
-	/// Accept a new network connection.
-	Accept(Channel<Command>),
-	
-	// TODO: add other useful control messages
-	
-	/// Checks the time since the last contact to the leader
-	CheckForTimeout {},
+    /// Open an account with a unique name.
+    Open { account: String },
 
-	/// After a timeout wait for another 1-299 ms before starting an election
-	Timeout {},
+    /// Deposit money into an account.
+    Deposit { account: String, amount: usize },
 
-	/// Start an election
-	Election{},
-	/// Call to vote
-	ElectMe { candidate_id: usize, last_entry_term: usize, last_entry_index: usize },
-	/// Accept candidate
-	VoteYes { origin_id: usize },
-	/// Reject candidate
-	VoteNo { origin_id: usize },
+    /// Withdraw money from an account.
+    Withdraw { account: String, amount: usize },
 
-	/// Periodically sending heartbeats
-	SendingHeartbeat{},
+    /// Transfer money between accounts.
+    Transfer {
+        src: String,
+        dst: String,
+        amount: usize,
+    },
 
-	/// Heartbeat -> TODO add payload
-	HeartBeat { leader_term: usize },
-	Append { current_command: Box<Command>, last_command: Box<Command> },
+    /// Accept a new network connection.
+    Accept(Channel<Command>),
 
-	//Commit
+    // TODO: add other useful control messages
+    /// Checks the time since the last contact to the leader
+    CheckForTimeout {},
 
+    /// After a timeout wait for another 1-299 ms before starting an election
+    Timeout {},
+
+    /// Start an election
+    Election {},
+
+    /// Call to vote
+    ElectMe {
+        candidate_id: usize,
+        last_entry_term: usize,
+        last_entry_index: usize,
+    },
+
+    /// Accept candidate
+    VoteYes { origin_id: usize },
+
+    /// Reject candidate
+    VoteNo { origin_id: usize },
+
+    /// Periodically sending heartbeats
+    SendingHeartbeat {},
+
+    /// Heartbeat -> TODO add payload
+    HeartBeat { leader_term: usize },
+
+    Append {
+        current_command: Box<Command>,
+        last_command: Box<Command>,
+    },
+    //Commit
 }
 
 // TODO: add other useful structures and implementations
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum State {
-	Leader,
-	Candidate,
-	Follower
+    Leader,
+    Candidate,
+    Follower,
 }
 
-
 /// Helper macro for defining test-scenarios.
-/// 
+///
 /// The basic idea is to write test-cases and then observe the behavior of the
 /// simulated network through the tracing mechanism for debugging purposes.
-/// 
+///
 /// The macro defines a mini-language to easily express sequences of commands
 /// which are executed concurrently unless you explicitly pass time between them.
 /// The script needs some collection of channels to operate over which has to be
@@ -69,9 +79,9 @@ pub enum State {
 /// Commands are separated by semicolons and are either requests (open an
 /// account, deposit money, withdraw money and transfer money between accounts)
 /// or other commands (currently only sleep).
-/// 
+///
 /// # Examples
-/// 
+///
 /// The following script creates two accounts (Foo and Bar) in different branch
 /// offices, deposits money in the Foo-account, waits a second, transfers it to
 /// bar, waits another half second and withdraws the money. The waiting periods
@@ -79,7 +89,7 @@ pub enum State {
 /// the sequence of transactions before referring to changes made in a different
 /// branch office. Within one branch office the timing is not important since
 /// the commands are always delivered in sequence.
-/// 
+///
 /// ```rust
 ///     let channels: Vec<Channel<_>>;
 ///     script! {
@@ -92,11 +102,14 @@ pub enum State {
 ///         [1] "Bar" => withdraw(10);
 ///     }
 /// ```
+///
+///
+///
 #[macro_export]
 macro_rules! script {
 	// empty base case
 	(@expand $chan_vec:ident .) => {};
-	
+
 	// meta-rule for customer requests
 	(@expand $chan_vec:ident . [$id:expr] $acc:expr => $($cmd:ident($($arg:expr),*)),+; $($tail:tt)*) => {
 		$(
@@ -106,13 +119,13 @@ macro_rules! script {
 		)*
 		script! { @expand $chan_vec . $($tail)* }
 	};
-	
+
 	// meta-rule for other commands
 	(@expand $chan_vec:ident . $cmd:ident($($arg:expr),*); $($tail:tt)*) => {
 		script! { @command $cmd($($arg),*) }
 		script! { @expand $chan_vec . $($tail)* }
 	};
-	
+
 	// customer requests
 	(@request open($holder:expr,)) => {
 		$crate::protocol::Command::Open {
@@ -138,7 +151,7 @@ macro_rules! script {
 			amount: $amount
 		}
 	};
-	
+
 	// other commands
 	(@command sleep($time:expr)) => {
 		std::thread::sleep(std::time::Duration::from_millis(($time as f64 * 1000.0) as u64));
@@ -146,13 +159,13 @@ macro_rules! script {
 	(@command sleep()) => {
 		std::thread::sleep(std::time::Duration::from_millis(1000));
 	};
-	
+
 	// entry point for the user
 	(use $chan_vec:expr; $($tail:tt)*) => {
 		let ref channels = $chan_vec;
-		script! { @expand channels . $($tail)* }	
+		script! { @expand channels . $($tail)* }
 	};
-	
+
 	// rudimentary error diagnostics
 	(@request $cmd:ident $($tail:tt)*) => {
 		compile_error!("maybe you mean one of open, deposit, withdraw or transfer?")
